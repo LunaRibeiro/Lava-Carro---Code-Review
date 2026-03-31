cf2c3687f1f78ea62be3f4a168555d7481a17f38
Luna <86332474+LunaRibeiro@users.noreply.github.com>
2026-03-31 17:10:44 -0300

    refactor: código completamente arrumado

--- codigo.c
+++ codigo.c
@@ -7,136 +7,188 @@ struct Carro {
     char cor[30];
 };
 
-int decrementarFila(int *tamanhoFila);
-int incrementarFila(int *tamanhoFila);
-void limparEntrada();
-float calcularValor(float preco, float quantidade);
-int validarEntradas(float precoEtanol, float precoGasolina, float precoAditivada, float capacidadeFila);
-int mostrarMenu();
+void limparEntrada() {
+    int ch;
+    while ((ch = getchar()) != '\n' && ch != EOF);
+}
 
-int main(void) {
+int mostrarMenu() {
+    int opcao;
 
-    float precoEtanol = 1, precoGasolina = 1, precoAditivada = 1;
-    float litrosEtanol = 0, litrosGasolina = 0, litrosAditivada = 0;
-    float valorArrecadado = 0;
-    float carrosAtendidos = 0;
-    float capacidadeFila = 1;
+    printf("\n================ MENU ================\n");
+    printf("1 - Adicionar carro na fila\n");
+    printf("2 - Abastecer carro\n");
+    printf("3 - Chamar próximo carro\n");
+    printf("4 - Relatórios\n");
+    printf("5 - Sair\n");
+    printf("=====================================\n");
+    printf("Escolha uma opção: ");
+    
+    scanf("%d", &opcao);
+    return opcao;
+}
 
-    float etanolRestante = 200, gasolinaRestante = 200, aditivadaRestante = 200;
+int validarEntradas(float etanol, float gasolina, float aditivada, int capacidadeFila) {
+    if (etanol < 0 || gasolina < 0 || aditivada < 0 || capacidadeFila <= 0) {
+        printf("\nValores inválidos! Tente novamente.\n");
+        return 1;
+    }
+    return 0;
+}
 
-    int tamanhoFila = 0, opcaoMenu, opcaoRelatorio;
-    int capacidadeMaxima = 100;
-    int indiceCarro = 0;
+int main() {
+    float precoEtanol, precoGasolina, precoAditivada;
+
+    float totalEtanol = 0, totalGasolina = 0, totalAditivada = 0;
+
+    // Estoque
+    float etanolRestante = 200;
+    float gasolinaRestante = 200;
+    float aditivadaRestante = 200;
+
+    // Controle da fila
+    int capacidadeFila;
+    int tamanhoFila = 0;
+    int carrosAtendidos = 0;
+
+    // Controle de fila (FIFO)
+    int inicioFila = 0;
+    int fimFila = 0;
 
     struct Carro carros[100];
 
     do {
-        validarEntradas(precoEtanol, precoGasolina, precoAditivada, capacidadeFila);
-
         printf("\nDigite o preço do etanol: ");
         scanf("%f", &precoEtanol);
 
-        printf("\nDigite o preço da gasolina: ");
+        printf("Digite o preço da gasolina: ");
         scanf("%f", &precoGasolina);
 
-        printf("\nDigite o preço da gasolina aditivada: ");
+        printf("Digite o preço da gasolina aditivada: ");
         scanf("%f", &precoAditivada);
 
-        printf("\nInforme o tamanho da fila: ");
-        scanf("%f", &capacidadeFila);
+        printf("Informe a capacidade da fila: ");
+        scanf("%d", &capacidadeFila);
 
-    } while (precoEtanol < 0 || precoGasolina < 0 || precoAditivada < 0 || capacidadeFila <= 0);
+    } while (validarEntradas(precoEtanol, precoGasolina, precoAditivada, capacidadeFila));
+
+    int opcao;
 
     do {
-        opcaoMenu = mostrarMenu();
+        opcao = mostrarMenu();
 
-        switch (opcaoMenu) {
-
+        switch (opcao) {
             case 1:
                 if (tamanhoFila < capacidadeFila) {
-                    incrementarFila(&tamanhoFila);
+
                     limparEntrada();
 
                     printf("\nModelo: ");
-                    fgets(carros[indiceCarro].modelo, 30, stdin);
+                    fgets(carros[fimFila].modelo, 30, stdin);
 
-                    printf("\nCor: ");
-                    fgets(carros[indiceCarro].cor, 30, stdin);
+                    printf("Cor: ");
+                    fgets(carros[fimFila].cor, 30, stdin);
 
-                    printf("\nPlaca: ");
-                    fgets(carros[indiceCarro].placa, 30, stdin);
+                    printf("Placa: ");
+                    fgets(carros[fimFila].placa, 30, stdin);
 
-                    indiceCarro++;
-                    printf("\nCarro adicionado\n");
+                    fimFila++;
+                    tamanhoFila++;
+
+                    printf("\nCarro adicionado à fila!\n");
+
                 } else {
-                    printf("\nFila cheia\n");
+                    printf("\nFila cheia!\n");
                 }
                 break;
 
             case 2:
                 if (tamanhoFila > 0) {
 
-                    printf("\nEtanol: ");
-                    scanf("%f", &litrosEtanol);
-                    etanolRestante -= litrosEtanol;
+                    int tipoCombustivel;
+                    float litros;
 
-                    printf("\nGasolina: ");
-                    scanf("%f", &litrosGasolina);
-                    gasolinaRestante -= litrosGasolina;
+                    printf("\nEscolha o combustível:\n");
+                    printf("1 - Etanol\n");
+                    printf("2 - Gasolina\n");
+                    printf("3 - Aditivada\n");
+                    printf("Opção: ");
+                    scanf("%d", &tipoCombustivel);
 
-                    printf("\nAditivada: ");
-                    scanf("%f", &litrosAditivada);
-                    aditivadaRestante -= litrosAditivada;
+                    printf("Quantidade de litros: ");
+                    scanf("%f", &litros);
+
+                    switch (tipoCombustivel) {
+                        case 1:
+                            totalEtanol += litros;
+                            etanolRestante -= litros;
+                            printf("Abastecido com Etanol\n");
+                            break;
+
+                        case 2:
+                            totalGasolina += litros;
+                            gasolinaRestante -= litros;
+                            printf("Abastecido com Gasolina\n");
+                            break;
+
+                        case 3:
+                            totalAditivada += litros;
+                            aditivadaRestante -= litros;
+                            printf("Abastecido com Aditivada\n");
+                            break;
+
+                        default:
+                            printf("Opção inválida!\n");
+                            break;
+                    }
 
                     carrosAtendidos++;
-                    decrementarFila(&tamanhoFila);
+                    tamanhoFila--;
+                    inicioFila++;
 
                 } else {
-                    printf("Fila vazia\n");
+                    printf("\nFila vazia!\n");
+                }
+                break;
+            case 3:
+                if (tamanhoFila > 0) {
+                    printf("\nChamando próximo carro...\n");
+                } else {
+                    printf("\nFila vazia!\n");
                 }
                 break;
 
             case 4:
-                printf("\nCarros atendidos: %.0f\n", carrosAtendidos);
+                printf("\n=========== RELATÓRIO ===========\n");
+
+                printf("\nLitros vendidos:\n");
+                printf("Etanol: %.2f\n", totalEtanol);
+                printf("Gasolina: %.2f\n", totalGasolina);
+                printf("Aditivada: %.2f\n", totalAditivada);
+
+                printf("\nValor arrecadado:\n");
+                printf("Etanol: %.2f\n", totalEtanol * precoEtanol);
+                printf("Gasolina: %.2f\n", totalGasolina * precoGasolina);
+                printf("Aditivada: %.2f\n", totalAditivada * precoAditivada);
+
+                printf("\nEstoque restante:\n");
+                printf("Etanol: %.2f\n", etanolRestante);
+                printf("Gasolina: %.2f\n", gasolinaRestante);
+                printf("Aditivada: %.2f\n", aditivadaRestante);
+
+                printf("\nCarros atendidos: %d\n", carrosAtendidos);
+                printf("=================================\n");
                 break;
 
+            case 5:
+                printf("\nEncerrando programa...\n");
+                break;
+
+            default:
+                printf("\nOpção inválida!\n");
         }
 
-    } while (opcaoMenu != 5);
+    } while (opcao != 5);
 
     return 0;
-}
-
-float calcularValor(float preco, float quantidade) {
-    return preco * quantidade;
-}
-
-int validarEntradas(float precoEtanol, float precoGasolina, float precoAditivada, float capacidadeFila) {
-    if (precoEtanol < 0) printf("Etanol inválido\n");
-    if (precoGasolina < 0) printf("Gasolina inválida\n");
-    if (precoAditivada < 0) printf("Aditivada inválida\n");
-    if (capacidadeFila <= 0) printf("Fila inválida\n");
-    return 0;
-}
-
-int mostrarMenu() {
-    int opcao;
-    printf("\n1-Adicionar\n2-Abastecer\n3-Próximo\n4-Relatório\n5-Sair\n");
-    scanf("%d", &opcao);
-    return opcao;
-}
-
-void limparEntrada() {
-    int ch;
-    while ((ch = fgetc(stdin)) != EOF && ch != '\n') {}
-}
-
-int incrementarFila(int *tamanhoFila) {
-    (*tamanhoFila)++;
-    return *tamanhoFila;
-}
-
-int decrementarFila(int *tamanhoFila) {
-    (*tamanhoFila)--;
-    return *tamanhoFila;
 }
